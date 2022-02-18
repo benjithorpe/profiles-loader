1db47b2ddddbd8c8aa1e4cb1acd83246faa749b8
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-02-18 23:18:19 +0000

    Get random data from randomuser.me using fetchAPI

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# single-page-portfolio
-A simple single page portfolio built with builm.io
+# Profiles Loader
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+A simple single page site that loads random user profiles.
+
+
+## Tech Stack
+- **Client:** HTML5, CSS3, Bulma
+- **Server:** [![Netlify Status](https://api.netlify.com/api/v1/badges/d8b79ce3-b020-42b2-8c1f-f1f55c16e754/deploy-status)](https://app.netlify.com/sites/profiles-loader/deploys)
+
+## Features
+- Load data dynamically
+- Load extra data on button press
+
+Clone the project
+```bash
+  git clone https://github.com/benjithorpe/profiles-loader.git
+```
+
+## Screenshots
+![App Screenshot]()
+
+
+## Lessons Learned
+
+Learned
+  - using the fetch API
+  - using the `template` tag and JS `cloneNode()` to clone HTML data
+Challenges
+  - Learning Bulma indepth
+  - Using `async/await` features in JS
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,44 @@
 const template = document.querySelector('[data-template]');
-const profileList = document.querySelector('[data-profile-wrapper]');
+const profileListWrapper = document.querySelector('[data-profile-wrapper]');
 
 // Generates the profile cards to be used
-function generateProfiles(amountOfProfile) {
+function generateProfiles(profileList = []) {
 
-  for (let i = 0; i < amountOfProfile; i++) {
+  for (let i = 0; i < profileList.length; i++) {
     // clone the template, then append to the list
     const item = template.content.cloneNode(true);
-    profileList.append(item);
+    // console.log(profileList[i])
+
+    const profileData = {
+      name: `${profileList[i].name.title}. ${profileList[i].name.first} ${profileList[i].name.last}`,
+      email: profileList[i].email,
+      contact: profileList[i].phone,
+      age: profileList[i].dob.age,
+      address: `${profileList[i].location.city}, ${profileList[i].location.country}`,
+      picture: profileList[i].picture.medium,
+    }
+
+    item.querySelector(".age").textContent = `${profileData.age} years`
+    item.querySelector(".name").textContent = `${profileData.name}`
+    item.querySelector(".email").textContent = `${profileData.email}`
+    item.querySelector(".contact").textContent = `${profileData.contact}`
+    item.querySelector(".address").textContent = `${profileData.address}`
+    item.querySelector("figure.user-img img").setAttribute("src", `${profileData.picture}`)
+
+    // Add the new generated template in the HTML
+    profileListWrapper.append(item);
   }
 }
 
-generateProfiles(5);
+// Builds a profile card with the given arguments passed in
+function buildTemplateData(item, profileData) {}
 
 const RANDOM_USER_URL = "https://randomuser.me/api/1.3/?results=10";
 
-// fetch(RANDOM_USER_URL)
-// .then(response => response.json())
-// .then(data => console.log(data.results))
-// .catch(error => console.log("An error occurred: " + error));
+fetch(RANDOM_USER_URL)
+  .then(response => response.json())
+  .then(data => {
+    generateProfiles(data.results);
+    console.log(data.results);
+  })
+  .catch(error => console.log("An error: " + error));
